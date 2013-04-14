5f30375a20d03abfb81adf2791e12456c0104307
astral451 <Nathan.turner411@gmail.com>
2013-04-13 21:00:07 -0500

    Finished mkyong tutorial on making a button

--- FirstApp/src/com/mycompany/firstapp/MainActivity.java
+++ FirstApp/src/com/mycompany/firstapp/MainActivity.java
@@ -4,15 +4,39 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.net.Uri;
+import android.content.Intent;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity
 {
+
+	Button button;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		int x = 1;
+
+		addListenerOnButton();
     }
+	
+	public void addListenerOnButton( ){
+		
+		button = (Button) findViewById( R.id.button_01 );
+		
+		button.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick( View arg0 ) {
+				
+				Intent browserIntent = 
+					new Intent( Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com" ) );
+				startActivity( browserIntent );
+			}
+		} );
+		
+	}
 }
